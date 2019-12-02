35060b1a57a3a94649f3a13ae19f69cc1faad16e
maciejgawlowski <Mx3va111>
2019-12-02 21:24:17 +0100

    table init

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,18 @@
-/* main js file */
-
 const a = 'hello';
 console.log(a);
 console.log('dupa');
+
+$(document).ready(function () {
+
+    console.log('whatever');
+
+});
+
+//show or hide table row details
+$(".table-row").click(function () {
+        if ($(this).find('.table-row-details').is(":hidden")) {
+            $(this).find('.table-row-details').slideDown("slow");
+        } else {
+            $(this).find('.table-row-details').hide();
+        }
+});
